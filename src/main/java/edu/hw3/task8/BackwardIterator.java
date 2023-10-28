161a32afe5afc05c144b7005be901d67b4333404
Artemiy Rodionov <artemiyrodionov39@gmail.com>
2023-10-28 22:03:05 +0300

    add list overload that doesn't copy collection in task 8

--- src/main/java/edu/hw3/task8/BackwardIterator.java
+++ src/main/java/edu/hw3/task8/BackwardIterator.java
@@ -7,10 +7,17 @@ import java.util.ListIterator;
 import java.util.Objects;
 
 public class BackwardIterator<E> implements Iterator<E> {
+    private static final String COLLECTION_CANNOT_BE_NULL = "collection cannot be null";
     private final ListIterator<E> iterator;
 
     public BackwardIterator(Collection<E> collection) {
-        iterator = List.copyOf(Objects.requireNonNull(collection)).listIterator(collection.size());
+        Objects.requireNonNull(collection, COLLECTION_CANNOT_BE_NULL);
+        iterator = List.copyOf(collection).listIterator(collection.size());
+    }
+
+    public BackwardIterator(List<E> list) {
+        Objects.requireNonNull(list, COLLECTION_CANNOT_BE_NULL);
+        iterator = list.listIterator(list.size());
     }
 
     @Override
